d2ac0a3e437bcf5df2506407a988486c1751f490
lealrx <lleroux29@gmail.com>
2020-03-11 15:30:29 +0100

    sans bug Controller/Pom + application prop/server

--- src/main/java/com/inti/restController/UtilisateurController.java
+++ src/main/java/com/inti/restController/UtilisateurController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.inti.entities.Utilisateur;
+import com.inti.service.interfaces.IUtilisateurService;
 
 @CrossOrigin
 @RestController
